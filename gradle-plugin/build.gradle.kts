bd5aa131b1fa1c0b706728a808dd5e3f6a120f94
Lammert Westerhoff <westerhoff@gmail.com>
2020-12-15 11:00:10 -0600

    Restore dependencies

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -10,6 +10,26 @@ gradlePlugin {
     }
 }
 
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib", "1.4.20"))
+
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
 // Add a source set for the functional test suite
 val functionalTestSourceSet = sourceSets.create("functionalTest") {
 }
