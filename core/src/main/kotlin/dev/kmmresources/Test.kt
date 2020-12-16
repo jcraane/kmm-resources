b8e82fefb959a139c3b939edcd3992d41cf940ac
jamiecraane <jcraane@gmail.com>
2020-12-16 16:39:03 +0100

    dd Test.kt

---
+++ core/src/main/kotlin/dev/kmmresources/Test.kt
@@ -0,0 +1,12 @@
+package dev.kmmresources
+
+fun main() {
+    val dict: Map<String, Any> = mapOf("a" to "A")
+    val dict2: Map<String, Any> = mapOf("a" to mapOf("b" to "a"))
+
+    val newDict = dict as Map<String, Map<String, Any>>
+    println(newDict["a"] is Map<String, Any>)
+
+    val newDict2 = dict2 as Map<String, Map<String, Any>>
+    println(newDict2["a"] is Map<String, Any>)
+}
