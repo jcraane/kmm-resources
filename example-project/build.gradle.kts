bb0f660d08e22b35f698790e464141b2e9ba5a08
jamiecraane <jcraane@gmail.com>
2020-12-16 09:10:52 +0100

    Remove unused dependencies

--- example-project/build.gradle.kts
+++ example-project/build.gradle.kts
@@ -15,9 +15,6 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:29.0-jre")
-
     implementation(kotlin("stdlib", "1.4.20"))
 
     // Use the Kotlin test library.
@@ -25,9 +22,6 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
 }
 
 // Example project demonstrating plugin usage
