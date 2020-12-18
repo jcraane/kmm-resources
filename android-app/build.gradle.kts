127c4a653d7f63401090689559111b95d0368212
Lammert Westerhoff <westerhoff@gmail.com>
2020-12-18 10:27:31 -0600

    Do some work on iOS integration

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -102,7 +102,9 @@ val packForXcode by tasks.creating(Sync::class) {
     val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
     inputs.property("mode", mode)
     dependsOn(framework.linkTask)
-    val targetDir = File(buildDir, "xcode-frameworks")
+//    val targetDir = File(buildDir, "xcode-frameworks")
+    val targetDir = findProperty("configuration.build.dir")
+            ?: throw RuntimeException("configuration.build.dir is not defined. Please pass this property from the XCode build.")
     from({ framework.outputDirectory })
     into(targetDir)
 }
