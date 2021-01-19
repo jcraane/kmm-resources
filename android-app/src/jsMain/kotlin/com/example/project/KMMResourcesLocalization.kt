67d33bb537c6d51c1a53517d4c4bda1fe15a1692
Lammert Westerhoff <westerhoff@gmail.com>
2021-01-19 13:12:10 -0600

    generated

--- android-app/src/jsMain/kotlin/com/example/project/KMMResourcesLocalization.kt
+++ android-app/src/jsMain/kotlin/com/example/project/KMMResourcesLocalization.kt
@@ -2,34 +2,53 @@
 package com.example.project
 
 
-val localizations = emptyMutableMap<String, Map<String, String>>()
-var currentLanguage = "en"
+import kotlinx.browser.window
+
+var currentLanguage: String? = null
+val fallbackLanguage = "en"
 
 private fun getString(key: String, vararg formatArgs: String): String {
-    return localizations[currentLanguage][key]
+    val browserLanguage = window.navigator.languages.firstOrNull()
+    val language = currentLanguage ?: browserLanguage ?: fallbackLanguage
+    val languageKey = language.split('-')[0]
+    val languageLocalizations = localizations[language] ?: localizations[languageKey] ?: localizations[fallbackLanguage]
+    return languageLocalizations?.let {
+        var message = it[key]
+        formatArgs.forEachIndexed { index, arg ->
+            message = message?.replace("%${index + 1}\$@", arg)
+        }
+        message
+    } ?: ""
 }
-actual fun L.ExampleContent.Pages.Page1.body(): String = getString("l.exampleContent.pages.page1.body") ?: ""
-actual fun L.ExampleContent.Pages.Page1.title(): String = getString("l.exampleContent.pages.page1.title") ?: ""
-actual fun L.ExampleContent.Pages.Page2.body(): String = getString("l.exampleContent.pages.page2.body") ?: ""
-actual fun L.ExampleContent.Pages.Page2.title(): String = getString("l.exampleContent.pages.page2.title") ?: ""
+
+actual fun L.ExampleContent.Pages.Page1.body(): String = getString("l.exampleContent.pages.page1.body")
+actual fun L.ExampleContent.Pages.Page1.title(): String = getString("l.exampleContent.pages.page1.title")
+actual fun L.ExampleContent.Pages.Page2.body(): String = getString("l.exampleContent.pages.page2.body")
+actual fun L.ExampleContent.Pages.Page2.title(): String = getString("l.exampleContent.pages.page2.title")
 actual fun L.ExampleContent.Pages.Page3.Body.checkmarks(): List<String> = emptyList()
-actual fun L.ExampleContent.Pages.Page3.title(): String = getString("l.exampleContent.pages.page3.title") ?: ""
-actual fun L.General.ampersand1(): String = getString("l.general.ampersand1") ?: ""
-actual fun L.General.ampersand2(): String = getString("l.general.ampersand2") ?: ""
-actual fun L.General.ampersand3(): String = getString("l.general.ampersand3") ?: ""
-actual fun L.General.another(): String = getString("l.general.another") ?: ""
-actual fun L.General.Button.ok(): String = getString("l.general.button.ok") ?: ""
-actual fun L.General.Error.invalidField(): String = getString("l.general.error.invalidField") ?: ""
-actual fun L.General.Error.tryAgainLater(): String = getString("l.general.error.tryAgainLater") ?: ""
-actual fun L.General.iDontKnow(): String = getString("l.general.iDontKnow") ?: ""
-actual fun L.General.no(): String = getString("l.general.no") ?: ""
-actual fun L.General.withParams(value0: String, value1: String): String = getString("l.general.withParams", value0, value1) ?: ""
-actual fun L.General.yes(): String = getString("l.general.yes") ?: ""
-actual fun L.Greetings.hello(): String = getString("l.greetings.hello") ?: ""
+actual fun L.ExampleContent.Pages.Page3.title(): String = getString("l.exampleContent.pages.page3.title")
+actual fun L.General.ampersand1(): String = getString("l.general.ampersand1")
+actual fun L.General.ampersand2(): String = getString("l.general.ampersand2")
+actual fun L.General.ampersand3(): String = getString("l.general.ampersand3")
+actual fun L.General.Button.ok(): String = getString("l.general.button.ok")
+actual fun L.General.Error.invalidField(): String = getString("l.general.error.invalidField")
+actual fun L.General.Error.tryAgainLater(): String = getString("l.general.error.tryAgainLater")
+actual fun L.General.iDontKnow(): String = getString("l.general.iDontKnow")
+actual fun L.General.no(): String = getString("l.general.no")
+actual fun L.General.withParams(value0: String, value1: String): String = getString("l.general.withParams", value0, value1)
+actual fun L.General.yes(): String = getString("l.general.yes")
+actual fun L.Greetings.hello(): String = getString("l.greetings.hello")
 actual fun L.Listofobjects.Categories.Samplecategory.Content.body(): String = ""
 actual fun L.Listofobjects.Categories.Samplecategory.Content.title(): String = ""
-actual fun L.Listofobjects.Categories.Samplecategory.detailedDescription(): String = getString("l.listofobjects.categories.samplecategory.detailedDescription") ?: ""
-actual fun L.Unit.gas(): String = getString("l.unit.gas") ?: ""
-actual fun L.Unit.Power.u3(): String = getString("l.unit.power.u3") ?: ""
-actual fun L.Unit.Power.u6(): String = getString("l.unit.power.u6") ?: ""
-actual fun L.Unit.Power.u9(): String = getString("l.unit.power.u9") ?: ""
+actual fun L.Listofobjects.Categories.Samplecategory.detailedDescription(): String = getString("l.listofobjects.categories.samplecategory.detailedDescription")
+actual fun L.Unit.gas(): String = getString("l.unit.gas")
+actual fun L.Unit.Power.u3(): String = getString("l.unit.power.u3")
+actual fun L.Unit.Power.u6(): String = getString("l.unit.power.u6")
+actual fun L.Unit.Power.u9(): String = getString("l.unit.power.u9")
+
+val localizations = {
+    val localizations = mutableMapOf<String, Map<String, String>>()
+	localizations["en"] = localizations_en()
+	localizations["nl"] = localizations_nl()
+   localizations
+}()
--- android-app/src/jsMain/kotlin/com/example/project/KMMResourcesLocalization_nl.kt
+++ android-app/src/jsMain/kotlin/com/example/project/KMMResourcesLocalization_nl.kt
@@ -1,36 +1,41 @@
 
 // Generated nl localization file\n
-localizations["nl"] = emptyMutableMap<String, String>()
+package com.example.project
 
 
-localizations["nl"]["l.exampleContent.pages.page1.body"] = "Body pagina 1";
-localizations["nl"]["l.exampleContent.pages.page1.title"] = "Titel pagina 1";
-localizations["nl"]["l.exampleContent.pages.page2.body"] = "- Dit is punt1
+internal fun localizations_nl(): Map<String, String> {
+    val localizations = mutableMapOf<String, String>()
+
+	localizations["l.exampleContent.pages.page1.body"] = "Body pagina 1"
+	localizations["l.exampleContent.pages.page1.title"] = "Titel pagina 1"
+	localizations["l.exampleContent.pages.page2.body"] = """
+- Dit is punt1
 - Dit is punt 2
 - Dit is punt 3
-";
-localizations["nl"]["l.exampleContent.pages.page2.title"] = "Body pagina 1";
-localizations["nl"]["l.exampleContent.pages.page3.body.checkmarks0"] = "24/7 telefonisch bereikbaar bij storingen";
-localizations["nl"]["l.exampleContent.pages.page3.body.checkmarks1"] = "Voorrijkosten en arbeidsloon inbegrepen";
-localizations["nl"]["l.exampleContent.pages.page3.title"] = "Titel pagina 3";
-localizations["nl"]["l.general.ampersand1"] = "Brood \u0026 boter";
-localizations["nl"]["l.general.ampersand2"] = "Brood & boter";
-localizations["nl"]["l.general.ampersand3"] = "Nota & betalen";
-localizations["nl"]["l.general.another"] = "OK";
-localizations["nl"]["l.general.button.ok"] = "OK";
-localizations["nl"]["l.general.error.invalidField"] = "Ongeldige waarde";
-localizations["nl"]["l.general.error.tryAgainLater"] = "Controleer de internetverbinding en probeer het opnieuw.";
-localizations["nl"]["l.general.iDontKnow"] = "Weet ik niet";
-localizations["nl"]["l.general.no"] = "Nee";
-localizations["nl"]["l.general.withParams"] = "Een %1$@ tekst met een %2$@";
-localizations["nl"]["l.general.yes"] = "Ja";
-localizations["nl"]["l.greetings.hello"] = "Hallo vanuit common code";
-localizations["nl"]["l.listofobjects.categories.samplecategory.content.0.body"] = "Dit is de body1";
-localizations["nl"]["l.listofobjects.categories.samplecategory.content.0.title"] = "Dit is de titel1";
-localizations["nl"]["l.listofobjects.categories.samplecategory.content.1.body"] = "Dit is de body2";
-localizations["nl"]["l.listofobjects.categories.samplecategory.content.1.title"] = "Dit is de titel2";
-localizations["nl"]["l.listofobjects.categories.samplecategory.detailedDescription"] = "Beschrijving van categorie";
-localizations["nl"]["l.unit.gas"] = "m³";
-localizations["nl"]["l.unit.power.u3"] = "kWh";
-localizations["nl"]["l.unit.power.u6"] = "MWh";
-localizations["nl"]["l.unit.power.u9"] = "GWh";
+"""
+	localizations["l.exampleContent.pages.page2.title"] = "Body pagina 1"
+	localizations["l.exampleContent.pages.page3.body.checkmarks0"] = "24/7 telefonisch bereikbaar bij storingen"
+	localizations["l.exampleContent.pages.page3.body.checkmarks1"] = "Voorrijkosten en arbeidsloon inbegrepen"
+	localizations["l.exampleContent.pages.page3.title"] = "Titel pagina 3"
+	localizations["l.general.ampersand1"] = "Brood \u0026 boter"
+	localizations["l.general.ampersand2"] = "Brood & boter"
+	localizations["l.general.ampersand3"] = "Nota & betalen"
+	localizations["l.general.button.ok"] = "OK"
+	localizations["l.general.error.invalidField"] = "Ongeldige waarde"
+	localizations["l.general.error.tryAgainLater"] = "Controleer de internetverbinding en probeer het opnieuw."
+	localizations["l.general.iDontKnow"] = "Weet ik niet"
+	localizations["l.general.no"] = "Nee"
+	localizations["l.general.withParams"] = "Een %1$@ tekst met een %2$@"
+	localizations["l.general.yes"] = "Ja"
+	localizations["l.greetings.hello"] = "Hallo vanuit common code"
+	localizations["l.listofobjects.categories.samplecategory.content.0.body"] = "Dit is de body1"
+	localizations["l.listofobjects.categories.samplecategory.content.0.title"] = "Dit is de titel1"
+	localizations["l.listofobjects.categories.samplecategory.content.1.body"] = "Dit is de body2"
+	localizations["l.listofobjects.categories.samplecategory.content.1.title"] = "Dit is de titel2"
+	localizations["l.listofobjects.categories.samplecategory.detailedDescription"] = "Beschrijving van categorie"
+	localizations["l.unit.gas"] = "m³"
+	localizations["l.unit.power.u3"] = "kWh"
+	localizations["l.unit.power.u6"] = "MWh"
+	localizations["l.unit.power.u9"] = "GWh"
+	return localizations
+}
